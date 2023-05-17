e3a830969c2b964517f18922881e274bc95051f3
donny333 <125969039+donny333@users.noreply.github.com>
2023-05-17 09:42:43 +0300

    Aprasytos pagrindines spalvos.

--- src/components/organisms/Header.jsx
+++ src/components/organisms/Header.jsx
@@ -2,14 +2,19 @@ import styled from "styled-components";
 import Logo from "../atoms/Logo";
 
 const StyledHeader = styled.header`
-
+    background: rgb(27,27,30);
+    background: linear-gradient(90deg, rgba(27,27,30,1) 0%, rgba(109,109,115,1) 100%);
+    height: 100px;
 `;
 
 const Header = () => {
     return ( 
-        <header>
+        <StyledHeader>
             <Logo />
-        </header>
+            <div>
+                
+            </div>
+        </StyledHeader>
     );
 }
  
